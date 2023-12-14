3082e218dc14919a9c8237e599f6ad4c77b4fc47
svachher <153861853+svachher@users.noreply.github.com>
2023-12-15 01:14:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// You can add JavaScript functionalities here
+
+// Example: Smooth scrolling to section
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
